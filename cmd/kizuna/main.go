b0a400193ea1dde1339850e2934c9807bc5a198b
shiron <shiron4710.dev@gmail.com>
2024-10-03 11:42:36 +0900

    feat: Impl config.go

--- cmd/kizuna/main.go
+++ cmd/kizuna/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"golang.org/x/net/http2"
@@ -8,15 +9,21 @@ import (
 
 	"github.com/halcyon-org/kizuna/gen/belifeline/v1/mainv1connect"
 	"github.com/halcyon-org/kizuna/internal/adapter/api"
+	"github.com/halcyon-org/kizuna/internal/infrastructure/repository/config"
 )
 
 func main() {
 	server := &api.BeLifelineServer{}
+	cfg := config.NewConfigRepository()
+	err := cfg.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
 	mux := http.NewServeMux()
 	path, handler := mainv1connect.NewBeLifelineServiceHandler(server)
 	mux.Handle(path, handler)
 	http.ListenAndServe(
-		"localhost:8080",
+		fmt.Sprintf("%s:%d", cfg.GetConfig().ListenAddr, cfg.GetConfig().Port),
 		// Use h2c so we can serve HTTP/2 without TLS.
 		h2c.NewHandler(mux, &http2.Server{}),
 	)
