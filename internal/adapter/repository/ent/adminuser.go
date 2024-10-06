f0de3d4b0b3d7771845684b2c93d3289d746667d
shiron <shiron4710.dev@gmail.com>
2024-10-07 00:43:11 +0900

    feat: Impl admin auth

--- internal/adapter/repository/ent/adminuser.go
+++ internal/adapter/repository/ent/adminuser.go
@@ -1,8 +1,14 @@
 package ent
 
-import "github.com/halcyon-org/kizuna/ent"
+import (
+	"context"
+
+	"github.com/halcyon-org/kizuna/ent"
+	"github.com/halcyon-org/kizuna/ent/adminuser"
+)
 
 type AdminUserRepository interface {
+	GetAdminUserByAPIKey(ctx context.Context, apiKey string) (*ent.AdminUser, error)
 }
 
 type adminUserRepositoryImpl struct {
@@ -13,4 +19,8 @@ func NewAdminUserRepository(db *ent.Client) AdminUserRepository {
 	return &adminUserRepositoryImpl{
 		DB: db,
 	}
+}
+
+func (r *adminUserRepositoryImpl) GetAdminUserByAPIKey(ctx context.Context, apiKey string) (*ent.AdminUser, error) {
+	return r.DB.AdminUser.Query().Where(adminuser.APIKey(apiKey)).Only(ctx)
 }
