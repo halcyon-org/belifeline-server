ff3718ebcc605a99f6d4dac716c4dcfa4b1f1764
Itsuki Shirahige <shiron4710.dev@gmail.com>
2024-10-07 17:19:40 +0900

    feat: Impl ClientCreate (#52)
    --- commit logs ---
    * feat: Impl ClientCreate

---
+++ internal/util/api.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+)
+
+func GenApiKey() string {
+	randBytes := make([]byte, 64)
+	_, err := io.ReadFull(rand.Reader, randBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return base64.RawURLEncoding.WithPadding(base64.NoPadding).EncodeToString(randBytes)
+}
