6211b80a3db7f2eeb8ba9d9975a64eda7e4f9129
6ouma <102800255+6ouma@users.noreply.github.com>
2024-10-23 12:56:09 +0900

    feat(admin): Implement the KoyoUpdate
    Update the existing koyo information.

--- internal/adapter/api/koyo.go
+++ internal/adapter/api/koyo.go
@@ -6,6 +6,7 @@ import (
 	connect "connectrpc.com/connect"
 	mainv1 "github.com/halcyon-org/kizuna/gen/belifeline/v1"
 	"github.com/halcyon-org/kizuna/gen/belifeline/v1/mainv1connect"
+	"github.com/halcyon-org/kizuna/internal/domain/domain"
 	"github.com/halcyon-org/kizuna/internal/usecase"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -21,8 +22,21 @@ func NewKoyoServiceHandler(KoyoInformationUsecase usecase.KoyoInformationUsecase
 	}
 }
 
-func (s *KoyoServiceHandlerImpl) KoyoUpdate(context.Context, *connect.Request[mainv1.KoyoUpdateRequest]) (*connect.Response[mainv1.KoyoUpdateResponse], error) {
-	return nil, status.Error(codes.Unimplemented, "method KoyoUpdate not implemented")
+func (s *KoyoServiceHandlerImpl) KoyoUpdate(ctx context.Context, req *connect.Request[mainv1.KoyoUpdateRequest]) (*connect.Response[mainv1.KoyoUpdateResponse], error) {
+	koyoInformation := req.Msg.KoyoInformation
+	if koyoInformation.FirstEntryAt != nil || koyoInformation.LastUpdatedAt != nil || koyoInformation.LastEntryAt != nil {
+		return nil, status.Error(codes.InvalidArgument, NewValidationError("time should not be set").Error())
+	}
+
+	data, err := s.KoyoInformationUsecase.UpdateKoyoInformation(ctx, koyoInformation)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	apiData := domain.ToAPIKoyoInformation(*data)
+	res := connect.NewResponse(&mainv1.KoyoUpdateResponse{KoyoInformation: &apiData})
+
+	return res, nil
 }
 
 func (s *KoyoServiceHandlerImpl) KoyoDataAdd(context.Context, *connect.Request[mainv1.KoyoDataAddRequest]) (*connect.Response[mainv1.KoyoDataAddResponse], error) {
