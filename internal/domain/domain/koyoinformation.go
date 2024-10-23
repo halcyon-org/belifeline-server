a6f277290e3a209e0112cc3c41c84031a4be7692
6ouma <102800255+6ouma@users.noreply.github.com>
2024-10-23 12:45:53 +0900

    feat(domain): Convert domain to API
    Return koyo information data by converting.

--- internal/domain/domain/koyoinformation.go
+++ internal/domain/domain/koyoinformation.go
@@ -3,7 +3,9 @@ package domain
 import (
 	"time"
 
+	v1 "github.com/halcyon-org/kizuna/gen/belifeline/models/v1"
 	"github.com/halcyon-org/kizuna/gen/ent"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type KoyoInformation struct {
@@ -35,5 +37,39 @@ func ToDomainKoyoInformation(e ent.KoyoInformation) KoyoInformation {
 		FirstEntryAt: e.FirstEntryAt,
 		LastEntryAt:  e.LastEntryAt,
 		UpdatedAt:    e.UpdatedAt,
+	}
+}
+
+func ToAPIKoyoInformation(d KoyoInformation) v1.KoyoInformation {
+	scales := make([]float32, len(d.Scales))
+	for i, scale := range d.Scales {
+		scales[i] = float32(scale)
+	}
+
+	var dataType v1.DataType
+	switch d.DataType {
+	case "image":
+		dataType = v1.DataType_DATA_TYPE_IMAGE
+	case "csv":
+		dataType = v1.DataType_DATA_TYPE_CSV
+	case "json":
+		dataType = v1.DataType_DATA_TYPE_JSON
+	default:
+		dataType = v1.DataType_DATA_TYPE_UNSPECIFIED
+	}
+
+	return v1.KoyoInformation{
+		KoyoId:          &v1.ULID{Value: d.ID},
+		KoyoName:        &d.Name,
+		KoyoDescription: &d.Description,
+		KoyoParams:      d.Params,
+		KoyoScales:      scales,
+		Version:         &v1.Version{Value: d.Version},
+		License:         &d.License,
+		DataType:        &dataType,
+		ApiKey:          &v1.ApiKey{Key: d.APIKey},
+		FirstEntryAt:    timestamppb.New(d.FirstEntryAt),
+		LastEntryAt:     timestamppb.New(d.LastEntryAt),
+		LastUpdatedAt:   timestamppb.New(d.UpdatedAt),
 	}
 }
