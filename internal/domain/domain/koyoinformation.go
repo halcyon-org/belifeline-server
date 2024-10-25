3c6a25a471cd6fce3e473b4458faceada3d76705
Souma <102800255+6ouma@users.noreply.github.com>
2024-10-25 10:41:41 +0900

    feat(admin): Implement the `KoyoCreate` (#108)
    Create a new koyo information.
    
    partially resolve #42
    
    --- commit logs ---
    * feat(usecase): Add a koyo information usecase
    * feat(domain): Convert domain to API
    * feat(admin): Implement the KoyoCreate
    * refactor(koyoinformation): Use the defined struct for data type

--- internal/domain/domain/koyoinformation.go
+++ internal/domain/domain/koyoinformation.go
@@ -3,7 +3,18 @@ package domain
 import (
 	"time"
 
+	v1 "github.com/halcyon-org/kizuna/gen/belifeline/models/v1"
 	"github.com/halcyon-org/kizuna/gen/ent"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type DataType string
+
+const (
+	DataTypeUnspecified DataType = "unspecified"
+	DataTypeImage       DataType = "image"
+	DataTypeCsv         DataType = "csv"
+	DataTypeJSON        DataType = "json"
 )
 
 type KoyoInformation struct {
@@ -14,7 +25,7 @@ type KoyoInformation struct {
 	Scales       []float64
 	Version      string
 	License      string
-	DataType     string
+	DataType     DataType
 	APIKey       string
 	FirstEntryAt time.Time
 	LastEntryAt  time.Time
@@ -30,10 +41,44 @@ func ToDomainKoyoInformation(e ent.KoyoInformation) KoyoInformation {
 		Scales:       e.Scales,
 		Version:      e.Version,
 		License:      e.License,
-		DataType:     string(e.DataType),
+		DataType:     DataType(e.DataType),
 		APIKey:       e.APIKey,
 		FirstEntryAt: e.FirstEntryAt,
 		LastEntryAt:  e.LastEntryAt,
 		UpdatedAt:    e.UpdatedAt,
+	}
+}
+
+func ToAPIKoyoInformation(d KoyoInformation) v1.KoyoInformation {
+	scales := make([]float32, len(d.Scales))
+	for i, scale := range d.Scales {
+		scales[i] = float32(scale)
+	}
+
+	var dataType v1.DataType
+	switch d.DataType {
+	case DataTypeUnspecified:
+		dataType = v1.DataType_DATA_TYPE_UNSPECIFIED
+	case DataTypeImage:
+		dataType = v1.DataType_DATA_TYPE_IMAGE
+	case DataTypeCsv:
+		dataType = v1.DataType_DATA_TYPE_CSV
+	case DataTypeJSON:
+		dataType = v1.DataType_DATA_TYPE_JSON
+	}
+
+	return v1.KoyoInformation{
+		KoyoId:          &v1.ULID{Value: d.ID},
+		KoyoName:        &d.Name,
+		KoyoDescription: &d.Description,
+		KoyoParams:      d.Params,
+		KoyoScales:      scales,
+		Version:         &v1.Version{Value: d.Version},
+		License:         &d.License,
+		DataType:        &dataType,
+		ApiKey:          &v1.ApiKey{Key: d.APIKey},
+		FirstEntryAt:    timestamppb.New(d.FirstEntryAt),
+		LastEntryAt:     timestamppb.New(d.LastEntryAt),
+		LastUpdatedAt:   timestamppb.New(d.UpdatedAt),
 	}
 }
